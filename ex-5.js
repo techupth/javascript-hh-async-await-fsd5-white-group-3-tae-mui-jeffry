f79cc8b357072722ef37849969509d4dc0e112cf
Astrick <mypiarenoo@gmail.com>
2023-12-15 06:19:29 +0700

    jeffry has been finished this exercise

--- ex-5.js
+++ ex-5.js
@@ -1,14 +1,14 @@
 // Exercise #5
 let getJohnProfile = () => {
-  return new Promise(function (_, reject) {
-    setTimeout(
-      () =>
-        reject({
-          errorCode: 500,
-          message: "👿 Failed to request data from server",
-        }),
-      2000
-    );
-  });
-};
-// Start coding here
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject({
+        errorCode: 500,
+        message: "👿 Failed to request data from server",
+      })
+    }, 2000);
+  })
+}
+getJohnProfile().then((result) => {
+  console.log(result)
+})
--- ex-6.js
+++ ex-6.js
@@ -1,35 +1,36 @@
 // Exercise #6
-let getJohnProfile = () => {
-  return new Promise(function (resolve) {
-    setTimeout(
-      () =>
-        resolve({
-          name: "John",
-          age: 20,
-          hobbies: ["Coding", "Football"],
-        }),
-      1000
-    );
-  });
-};
+let getJohnProfile = (resolve) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        name: "John",
+        age: 20,
+        hobbies: ["Coding", "Football"],
+      })
+    }, 1000);
+  })
+}
 
 let getJohnOrders = () => {
-  return new Promise(function (resolve) {
-    setTimeout(
-      () =>
-        resolve([
-          {
-            orderId: "001",
-            items: ["apple", "banana"],
-          },
-          {
-            orderId: "002",
-            items: ["orange", "itim"],
-          },
-        ]),
-      1500
-    );
-  });
-};
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve([
+        {
+          orderId: "001",
+          items: ["apple", "banana"],
+        },
+        {
+          orderId: "002",
+          items: ["orange", "itim"],
+        },
+      ])
+    }, 1500);
+  })
+}
 
-// Start coding here
+getJohnProfile().then((result) => {
+  console.log(result)
+})
+getJohnOrders().then((result) => {
+  console.log(result)
+})
